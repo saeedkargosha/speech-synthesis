3db7a9f7cdec2a127d4101c46ee43a44836398a5
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-16 23:32:36 +0330

    Add getWordByCharIndexAndLength parser

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -3,3 +3,7 @@ export const parseContentIntoSentences = (content: string): string[] => {
   const matches = content.match(sentenceRegex) || [];
   return matches.map((match) => match.replace(/<\/?s>/g, '').trim());
 };
+
+export const getWordByCharIndexAndLength = (content: string, index: number, length: number): string => {
+  return content.substring(index, index + length);
+};
