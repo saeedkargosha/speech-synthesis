e71e5976c373159f4e8e5971197c18b25ca8a479
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-16 17:27:16 +0330

    Add fetchContent api

--- src/api/ssml.ts
+++ src/api/ssml.ts
@@ -1,10 +1,16 @@
-const getSSML = async () => `<speak>
-  Here is a number <w role='amazon:VBD'>read</w>
-  as a cardinal number:
-  <say-as >12345</say-as>.
-  Here is a word spelled out:
-  <say-as interpret-as='spell-out'>hello</say-as>.</speak>`;
+import { axiosInstance } from './axios-instance';
+
+/**
+ * Fetch the content from the api
+ */
+const fetchContent = async (): Promise<string> => {
+  const res = await axiosInstance({
+    method: 'GET',
+    url: '/content',
+  });
+  return res.data;
+};
 
 export const SSMLService = {
-  getSSML,
+  fetchContent,
 };
