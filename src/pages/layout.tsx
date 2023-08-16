d01a4302b2b6bde946c67eb800a0f39301464f27
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-16 17:28:01 +0330

    Update title of site

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 export default function RootLayout() {
   return (
     <main className='px-8 py-4'>
-      <h1 className='text-4xl mb-12'>{'Speech App'}</h1>
+      <h1 className='text-4xl mb-12'>{'Text to speech'}</h1>
       <Outlet />
     </main>
   );
