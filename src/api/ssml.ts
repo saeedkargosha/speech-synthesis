755953bc49c6690ce117acb891dc36c6aadb3e7d
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-16 17:37:20 +0330

    Fix ts response of fetch content

--- src/api/ssml.ts
+++ src/api/ssml.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from './axios-instance';
 /**
  * Fetch the content from the api
  */
-const fetchContent = async (): Promise<string> => {
+const fetchContent = async (): Promise<{ content: string }> => {
   const res = await axiosInstance({
     method: 'GET',
     url: '/content',
