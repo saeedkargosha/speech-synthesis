8923b0a9897619556013e7ba4873eb4e23f5e2ff
saeed kargosha <saeed.k@cosmopolis.co>
2023-08-16 23:38:15 +0330

    Refactor SpeechSynthesis component to use speech engine

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,30 @@
-import { useState, useEffect, FC } from 'react';
-import SpeechSynthesis from '@components/SpeechSynthesis';
+import { useState, useEffect } from 'react';
 import { SSMLService } from '@api/ssml';
+import { parseContentIntoSentences } from '@utils/parse';
+import { useSpeech } from '@hooks/useSpeech';
+import { Controls } from '@components/Controls';
+import { CurrentlyReading } from '@components/CurrentlyReading';
 
 export default function HomePage() {
-  const [ssmlData, setSSMLData] = useState<string>('');
+  const [sentences, setSentences] = useState<Array<string>>([]);
+  const { currentWord, currentSentence, controls } = useSpeech(sentences);
 
   useEffect(() => {
-    const fetchSSML = async () => {
-      const res = await SSMLService.getSSML();
-      setSSMLData(res);
-    };
-    fetchSSML();
+    async function getContent() {
+      const fetchedContent = await SSMLService.fetchContent();
+      const parsedData = parseContentIntoSentences(fetchedContent.content);
+      setSentences(parsedData);
+    }
+    getContent();
   }, []);
 
-  return <SpeechSynthesis value={ssmlData} />;
+  return (
+    <div className='block max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow space-y-6'>
+      <div>
+        <CurrentlyReading currentSentence={currentSentence} currentWord={currentWord} />
+      </div>
+      <div className='text-base text-neutral-600'>{sentences.map((sentence) => sentence)}</div>
+      <Controls controles={controls} />
+    </div>
+  );
 }
